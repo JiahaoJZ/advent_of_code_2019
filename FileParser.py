05317f239a6814b1b5a2e2682d6b218da072fc86
Jiahao Ji <jiahao.ji@alumnos.upm.es>
2019-12-01 13:04:40 +0100

    Deleted v1

--- FileParser.py
+++
@@ -1,3 +0,0 @@
-def get_file_contents(path):
-  f = open(path, 'r')
-  return f.read()
--- day_1.py
+++
@@ -1,8 +0,0 @@
-import FileParser
-
-def fuel_requiered(data):
-  for line in data:
-    print(line + ";")
-
-content = FileParser.get_file_contents("data_1_data.txt")
-fuel_requiered(content)
